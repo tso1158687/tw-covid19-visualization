8b2691b2d0ee948671fca53181de734ad4d83f9f
jason zheng <tso1158687@gmail.com>
2021-06-14 22:31:21 +0800

    finish chart

--- src/app/modules/chart/chart.module.ts
+++ src/app/modules/chart/chart.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MobilityComponent } from './components/mobility/mobility.component';
 import { ChartsModule } from 'ng2-charts';
+import { NbRadioModule } from '@nebular/theme';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [MobilityComponent],
-  imports: [CommonModule, ChartsModule],
+  imports: [CommonModule, ChartsModule,NbRadioModule,FormsModule],
   exports: [MobilityComponent],
 })
 export class ChartModule {}
